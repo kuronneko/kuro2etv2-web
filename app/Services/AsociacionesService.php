93b8d1744285f374bbf45f1af4b0f08a45cb899c
wdocker <wdocker@wdocker.com>
2024-03-06 13:19:04 -0300

    AsociacionesService

---
+++ app/Services/AsociacionesService.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Services;
+
+use Carbon\Carbon;
+
+class AsociacionesService
+{
+    public static function tieneRelacionAsociada($parent, $relationship)
+    {
+        return $parent->{$relationship}()->exists();
+    }
+}
